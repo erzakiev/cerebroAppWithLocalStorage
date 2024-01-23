f46978d2197a9f48df81b629b0958378f2b89889
Emile <e.r.zakiev@gmail.com>
2024-01-23 23:51:51 +0100

    try add expr level in hover

--- inst/shiny/v1.3/gene_expression/obj_projection_hover_info.R
+++ inst/shiny/v1.3/gene_expression/obj_projection_hover_info.R
@@ -7,13 +7,23 @@ expression_projection_hover_info <- reactive({
     expression_projection_cells_to_show()
   )
   # message('--> trigger "expression_projection_hover_info"')
+  exprsns <- expression_projection_expression_levels()
   if (
     !is.null(preferences[["show_hover_info_in_projections"]]) &&
     preferences[['show_hover_info_in_projections']] == TRUE
   ) {
+    exprsns <- exprsns[expression_projection_cells_to_show()]
     hover_info <- hover_info_projections()[expression_projection_cells_to_show()]
+    hover_info <- glue::glue(
+      "{hover_info}<br>",
+      "<b>{Expression level}</b>: {formatC(exprsns, format = 'f', big.mark = ',', digits = 3)}"
+    )
   } else {
     hover_info <- hover_info_projections()
+    hover_info <- glue::glue(
+      "{hover_info}<br>",
+      "<b>{Expression level}</b>: {formatC(exprsns, format = 'f', big.mark = ',', digits = 3)}"
+    )
   }
   # message(str(hover_info))
   return(hover_info)
