6ce79d64c13a515845e3493e288f3430788f3975
Emile <e.r.zakiev@gmail.com>
2024-01-23 13:26:33 +0100

    fix

--- inst/shiny/v1.3/gene_expression/out_coords_of_selected_cells.R
+++ inst/shiny/v1.3/gene_expression/out_coords_of_selected_cells.R
@@ -5,14 +5,13 @@ output[["expression_projection_selected_cells_coords"]] <- renderText({
   ## check selection
   ## ... selection has not been made or there is no cell in it
   if ( is.null(expression_projection_selected_cells_coords()) ) {
-    ## manually set counter to 0
-    number_of_selected_cells <- 0
+    ## return empty line
+    return(NULL)
     ## ... selection has been made and at least 1 cell is in it
   } else {
     ## get number of selected cells
-    coords_of_selected_cells <- expression_projection_selected_cells_coords() %>%
-      nrow() %>%
-      formatC(format = "f", big.mark = ",", digits = 0)
+    coords_of_selected_cells <- expression_projection_selected_cells_coords() #%>%
+      #formatC(format = "f", big.mark = ",", digits = 0)
   }
   ## prepare string to show
   
