dab19ef437a307905addf71a9d42868c9b1c34a0
Emile <e.r.zakiev@gmail.com>
2024-01-24 01:03:31 +0100

    remove diag

--- inst/shiny/v1.3/gene_expression/obj_projection_hover_info.R
+++ inst/shiny/v1.3/gene_expression/obj_projection_hover_info.R
@@ -8,18 +8,18 @@ expression_projection_hover_info <- reactive({
   )
   # message('--> trigger "expression_projection_hover_info"')
   exprsns <- expression_projection_expression_levels()
-  print('printing head(exprsns)')
-  print(head(exprsns))
+  #print('printing head(exprsns)')
+  #print(head(exprsns))
   if (
     !is.null(preferences[["show_hover_info_in_projections"]]) &&
     preferences[['show_hover_info_in_projections']] == TRUE
   ) {
     toShow <- expression_projection_cells_to_show()
-    print('printing again head(toShow)')
-    print(head(toShow))
+    #print('printing again head(toShow)')
+    #print(head(toShow))
     #exprsns <- exprsns[toShow]
-    print('printing again head(exprsns)')
-    print(head(exprsns))
+    #print('printing again head(exprsns)')
+    #print(head(exprsns))
     hover_info <- hover_info_projections()[toShow]
     hover_info <- glue::glue(
       "{hover_info}<br>",
@@ -32,8 +32,8 @@ expression_projection_hover_info <- reactive({
       "<b>Expression level</b>: {formatC(exprsns, format = 'f', big.mark = ',', digits = 3)}"
     )
   }
-  print('printing head of hover_info')
-  print(head(hover_info))
+  #print('printing head of hover_info')
+  #print(head(hover_info))
   # message(str(hover_info))
   return(hover_info)
 })
