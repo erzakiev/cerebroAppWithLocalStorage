345c647441ed471f05eadf9734c7cf1d85d25c7e
erzakiev <e.r.zakiev@gmail.com>
2024-03-25 12:49:23 +0100

    improving the appearance

--- inst/shiny/v1.3/shiny_UI.R
+++ inst/shiny/v1.3/shiny_UI.R
@@ -79,6 +79,17 @@ ui <- dashboardPage(
   ),
   dashboardBody(
     shinyjs::useShinyjs(),
+    tags$head(tags$style(HTML(
+      '.myClass { 
+        font-size: 20px;
+        line-height: 50px;
+        text-align: left;
+        font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;
+        padding: 0 15px;
+        overflow: hidden;
+        color: white;
+      }
+    '))),
     tags$script(HTML('
       $(document).ready(function() {
         $("header").find("nav").append(\'<span class="myClass"> Text Here </span>\');
