ac0e12dcacc17c368156d5730bd4eb718da565a6
Emile <e.r.zakiev@gmail.com>
2024-04-08 08:58:35 +0200

    fiks?

--- inst/shiny/v1.3/overview/obj_projection_selected_cells.R
+++ inst/shiny/v1.3/overview/obj_projection_selected_cells.R
@@ -9,19 +9,20 @@ overview_projection_selected_cells <- reactive({
   ## check selection
   if(is.null(plotly::event_data("plotly_brushing", source = "overview_projection")) ||
      length(plotly::event_data("plotly_brushing", source = "overview_projection")) == 0){
-    if (
-      is.null(plotly::event_data("plotly_selected", source = "overview_projection")) ||
-      length(plotly::event_data("plotly_selected", source = "overview_projection")) == 0
-    ) {
+    
       ## ... selection has not been made or there is no cell in it
       return(NULL)
       
-    } else {
+     } else {
       ## ... selection has been made and at least 1 cell is in it
       ## get number of selected cells
+      if (
+         is.null(plotly::event_data("plotly_selected", source = "overview_projection")) ||
+         length(plotly::event_data("plotly_selected", source = "overview_projection")) == 0
+      ){
       plotly::event_data("plotly_selected", source = "overview_projection") %>%
         dplyr::mutate(identifier = paste0(x, '-', y)) %>%
         return()
-    }
+      }
   }
 })
