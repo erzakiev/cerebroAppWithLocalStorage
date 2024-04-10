e2aab60838de2507b4d6f3acf15a4c0cc80cd03d
Emile <e.r.zakiev@gmail.com>
2024-04-10 17:16:49 +0200

    tototo

--- inst/shiny/v1.3/overview/out_details_selected_cells_plot.R
+++ inst/shiny/v1.3/overview/out_details_selected_cells_plot.R
@@ -5,6 +5,8 @@
 ##   - if numerical: box/violin plot
 ##----------------------------------------------------------------------------##
 output[["overview_details_selected_cells_plot"]] <- plotly::renderPlotly({
+  print('printing input[["overview_projection_to_display"]]')
+  print(input[["overview_projection_to_display"]])
   print('printing input[[overview_selected_cells_plot_select_variable]]')
   print(input[["overview_selected_cells_plot_select_variable"]])
   req(
