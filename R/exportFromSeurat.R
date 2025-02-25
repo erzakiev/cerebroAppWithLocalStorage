23f2bab0c24cbba36020aba2c0ec55e1f17db54b
Emile <e.r.zakiev@gmail.com>
2025-02-25 10:06:41 +0100

    Update exportFromSeurat.R
    putting back check no 4 to check if it was actually breaking

--- R/exportFromSeurat.R
+++ R/exportFromSeurat.R
@@ -99,6 +99,19 @@ exportFromSeurat <- function(
   }
   message('Check 3 completed successfully')
 
+  ## check version of Seurat object and stop if it is lower than 3
+  if ( object@version < 3 ) {
+    stop(
+      paste0(
+        "Provided Seurat object has version `", object@version, "` but must be at least 3.0."
+      ),
+      call. = FALSE
+    )
+  }
+  message('Check 4 completed successfully')
+
+
+
   ## `groups`
   if ( any(groups %in% names(object@meta.data) == FALSE ) ) {
     stop(
