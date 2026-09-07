9cb0a097b1dfd1b8a055a01766554f11d4e7fa27
Emile <e.r.zakiev@gmail.com>
2026-09-07 15:56:33 +0200

    Added option to download violin plot data

--- inst/shiny/v1.3/gene_expression/UI_expression_by_group.R
+++ inst/shiny/v1.3/gene_expression/UI_expression_by_group.R
@@ -21,7 +21,8 @@ output[["expression_by_group_UI"]] <- renderUI({
           choices = getGroups(),
           width = "100%"
         ),
-        plotly::plotlyOutput("expression_by_group")
+        plotly::plotlyOutput("expression_by_group"),
+        downloadButton("download_excel", "Download data in excel format")
       )
     )
   )
@@ -30,6 +31,28 @@ output[["expression_by_group_UI"]] <- renderUI({
 ##----------------------------------------------------------------------------##
 ## Violin/box plot.
 ##----------------------------------------------------------------------------##
+
+output[['download_excel']] <- downloadHandler(
+  filename = function() {
+    paste0("data_", Sys.Date(), ".xlsx")
+  },
+  content = function(file) {
+    openxlsx::write.xlsx(
+      {
+        cells_df <- expression_projection_data()
+      if (is.list(expression_projection_expression_levels())) {
+        cells_df$expression_level <- do.call(cbind, expression_projection_expression_levels()) %>%
+          Matrix::rowMeans()
+      } else {
+        cells_df$expression_level <- expression_projection_expression_levels()
+      }
+      cells_df
+      },
+      file
+    )
+  }
+)
+
 
 output[["expression_by_group"]] <- plotly::renderPlotly({
   req(
